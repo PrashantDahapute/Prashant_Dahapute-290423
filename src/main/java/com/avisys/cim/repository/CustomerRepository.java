26a2c6529c22cd892a681c869b625f75402ec6fb
Prashant Dahapute <dahaputep@gmail.com>
2023-04-30 12:25:30 +0530

    add customer api is added . if number is already exist then it will give an error message and if not it will get added..

--- src/main/java/com/avisys/cim/repository/CustomerRepository.java
+++ src/main/java/com/avisys/cim/repository/CustomerRepository.java
@@ -24,4 +24,6 @@ public interface CustomerRepository extends JpaRepository<Customer,Long> {
 	
 	
 	
+	
+	
 }
--- src/main/java/com/avisys/cim/service/CustomerService.java
+++ src/main/java/com/avisys/cim/service/CustomerService.java
@@ -3,6 +3,8 @@ package com.avisys.cim.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,12 +30,25 @@ public class CustomerService {
 
 	public List<Customer> getByFirstName(String name) {
 		
-		return custRepo.findByFirstNameContainingIgnoreCase(name);
+		return custRepo.findByFirstNameContainingIgnoreCase(name); //this will check 
 	}
 	
 	public List<Customer> getByLastName(String name) {
 		
 		return custRepo.findByLastNameContainingIgnoreCase(name);
+	}
+
+	
+	//for sending the http response responseentity is uses to send http code and body containing message.
+	public ResponseEntity<String> createCustomer(Customer customer) {
+		Customer cust=custRepo.findByMobileNumber(customer.getMobileNumber());//check any customer is present		
+		//if cust is null that means there is no customer with this mobile number so add the customer.
+		if(cust!=null) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unable to create customer");
+		}else {
+			custRepo.save(customer);
+			return ResponseEntity.ok("Customer Added successfully");
+		}
 	}
 
 	
