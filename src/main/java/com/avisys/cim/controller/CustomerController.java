3486b84f2495ed1ef95e2f74c969bb02e2c3d924
Prashant Dahapute <dahaputep@gmail.com>
2023-04-30 21:24:31 +0530

    api is added to delete customer by mobile number

--- src/main/java/com/avisys/cim/controller/CustomerController.java
+++ src/main/java/com/avisys/cim/controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,12 +67,15 @@ public class CustomerController {
 		
 	}
 	
+	@DeleteMapping("/delete/{mobileNo}")
+	public ResponseEntity<String> deleteCustomer(@PathVariable String mobileNo){
+		
+		return custService.removeCustomer(mobileNo);
+	}
 	
 	@PostMapping("/add/{mobNo}")
 	public ResponseEntity<String> addCustomer(@RequestBody Customer customer,@PathVariable String mobNo){
 		return custService.createCustomer(customer,mobNo);
-		
-		
 	}
 
 }
--- src/main/java/com/avisys/cim/service/CustomerService.java
+++ src/main/java/com/avisys/cim/service/CustomerService.java
@@ -14,105 +14,95 @@ import com.avisys.cim.MobileNumber;
 import com.avisys.cim.repository.CustomerRepository;
 import com.avisys.cim.repository.MobileRepository;
 
-
 @Service
 @Transactional
 public class CustomerService {
-	
+
 	@Autowired
 	private CustomerRepository custRepo;
-	
+
 	@Autowired
 	private MobileRepository mobRepo;
-	
-	public List<Customer> getAll(){			
+
+	public List<Customer> getAll() {
 		return custRepo.findAll();
 	}
-	
+
 	public Customer getBymobileNumber(String mobileNumber) {
-		
-		List<MobileNumber> mobi=mobRepo.findAll();//getting all number present in the table
-		
-		for(MobileNumber mob:mobi) {
-			if(mob.getMobileNumber().equals(mobileNumber)) {
-				return mob.getCust();//return customer of the mobile number
+
+		List<MobileNumber> mobi = mobRepo.findAll();// getting all number present in the table
+
+		for (MobileNumber mob : mobi) {
+			if (mob.getMobileNumber().equals(mobileNumber)) {
+				return mob.getCust();// return customer of the mobile number
 			}
 		}
-		
+
 		return null;
 	}
 
 	public List<Customer> getByFirstName(String name) {
-		
-		return custRepo.findByFirstNameContainingIgnoreCase(name); //this will check 
+
+		return custRepo.findByFirstNameContainingIgnoreCase(name); // this will check
 	}
-	
+
 	public List<Customer> getByLastName(String name) {
-		
+
 		return custRepo.findByLastNameContainingIgnoreCase(name);
 	}
 
 	public String addNo(Long id, MobileNumber mob) {
 		try {
-		Customer cust=custRepo.findById(id).get();
-		cust.addnumber(mob);//this is helper method to add mobile object in list and set the 
-							//customer to the mobile number
-		mobRepo.save(mob);		
-		return "successful";
-		
-		}catch(Exception e) {
+			Customer cust = custRepo.findById(id).get();
+			cust.addnumber(mob);// this is helper method to add mobile object in list and set the
+								// customer to the mobile number
+			mobRepo.save(mob);
+			return "successful";
+
+		} catch (Exception e) {
 			return "Invalid Id";
 		}
 	}
 
-	
-	//this api return all mobile number a customer is having
+	// this api return all mobile number a customer is having
 	public List<MobileNumber> getAllNumber(Long id) {
-		
+
 		try {
-		Customer cust=custRepo.findById(id).get();
-		return cust.getList();
-		}catch(Exception e) {
+			Customer cust = custRepo.findById(id).get();
+			return cust.getList();
+		} catch (Exception e) {
 			return null;
 		}
 	}
-	
-	//for sending the http response responseentity is uses to send http code and body containing message.
-	public ResponseEntity<String> createCustomer(Customer customer,String mobNo) {
-		List<MobileNumber> mobi=mobRepo.findAll();
 
-		for(MobileNumber mob:mobi) {
-			
-			if(mob.getMobileNumber().equals(mobNo)) {
+	// for sending the http response responseentity is uses to send http code and
+	// body containing message.
+	public ResponseEntity<String> createCustomer(Customer customer, String mobNo) {
+		List<MobileNumber> mobi = mobRepo.findAll();
+
+		for (MobileNumber mob : mobi) {
+
+			if (mob.getMobileNumber().equals(mobNo)) {
 				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("already exists");
 			}
 		}
 		custRepo.save(customer);
-		return ResponseEntity.ok("Custmer addded Successfully");	
-		
+		return ResponseEntity.ok("Custmer addded Successfully");
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public ResponseEntity<String> removeCustomer(String mobileNo) {
+
+		List<MobileNumber> mobi = mobRepo.findAll();// getting all number present in the table
+
+		for (MobileNumber mob : mobi) {
+			if (mob.getMobileNumber().equals(mobileNo)) {
+				custRepo.delete(mob.getCust());
+				return ResponseEntity.ok("Custmer deleted Successfully");
+			}
+		}
+
+	 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("mobile number not exists");
+	}
+
 }
